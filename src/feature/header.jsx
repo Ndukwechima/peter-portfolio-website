c49613ff2b720bb5574e2cc3da979e7dae1722b7
pironoid14 <56125168+pironoid14@users.noreply.github.com>
2025-01-26 18:36:32 +0100

    adj

--- src/feature/header.jsx
+++ src/feature/header.jsx
@@ -2,7 +2,7 @@
 const Header = () => {
   return (
     <div className="h-12 w-full bg-teal-900 underline">
-    <header className="grid grid-cols-5 justify-items-center items-center gap-2 text-white py-2" >
+    <header className="grid grid-cols-5 justify-items-center items-center gap-4 text-white py-2" >
         <h1 className="col-start-1">Peter<span className="text-yellow-300">Dev</span></h1>
 
         <ul className="grid grid-cols-5 gap-4 col-start-2 col-end-7">

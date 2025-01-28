36542748a452ac5da94df27511a4ea3175d757ab
pironoid14 <56125168+pironoid14@users.noreply.github.com>
2025-01-28 18:05:59 +0100

    hero section done

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,12 +1,13 @@
 
+
 const Header = () => {
   return (
-    <div className="h-12 w-full bg-teal-900 underline">
+    <div className="h-12 w-full bg-[#10585A] underline">
     <header className="grid grid-cols-5 justify-items-center items-center gap-4 text-white py-2" >
-        <h1 className="col-start-1">Peter<span className="text-yellow-300">Dev</span></h1>
+        <h1 className="col-start-1">Peter<span className="text-[#FF9807]">Dev</span></h1>
 
         <ul className="grid grid-cols-5 gap-4 col-start-2 col-end-7">
-            <li className="text-yellow-300">Home</li>
+            <li className="text-[#FF9807]">Home</li>
             <li>About me</li>
             <li>Technology</li>
             <li>Projects</li>
